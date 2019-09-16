415a25b32f0c0b703b5bc8e861aec307a5f3b739
Matthieu CHARETTE <matthieu.charette@gmail.com>
2019-09-16 18:01:25 +0200

    Add files via upload

---
+++ Opencv.sh
@@ -0,0 +1,63 @@
+cd ~
+df -h
+sudo apt-get -y update && sudo apt-get -y upgrade
+sudo apt-get install -y build-essential cmake unzip pkg-config libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libcanberra-gtk* libatlas-base-dev gfortran python3-dev
+mkdir ~/Opencv
+cd ~/Opencv
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.0.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.0.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+rm opencv.zip
+rm opencv_contrib.zip
+mv opencv-4.0.0 opencv
+mv opencv_contrib-4.0.0 opencv_contrib
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo pip install virtualenv virtualenvwrapper
+sudo rm -rf ~/get-pip.py ~/.cache/pip
+echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.profile
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.profile
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.profile
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
+source ~/.profile
+cd ~/Opencv
+mkvirtualenv cv -p python3
+workon cv
+pip install numpy
+cd opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=~/Opencv/opencv_contrib/modules \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D BUILD_EXAMPLES=OFF ..
+sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile stop
+sudo /etc/init.d/dphys-swapfile start
+make -j4
+sudo make install
+sudo ldconfig
+sudo sed -i 's/CONF_SWAPSIZE=2048/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile stop
+sudo /etc/init.d/dphys-swapfile start
+cd ~/.virtualenvs/cv/lib/python3.7/site-packages/
+ln -s /usr/local/python/cv2/python-3.7/cv2.cpython-37m-arm-linux-gnueabihf.so cv2.so
+source ~/.profile
+cd ~
+workon cv
+OUTPUT=$(python3 -c 'import cv2
+print(cv2.__version__)')
+if [ "$OUTPUT" == '4.0.0' ]
+then
+    echo "Opencv 4.0.0 has been installed !"
+    exit 0
+else
+    echo "There are some errors so opencv 4.0.0 has not been installed correctly. Please try again !"
+    exit 1
+fi
